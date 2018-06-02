bc4fd35fc929ecdf31b97c850f7a5fe9093ee958
Gary Lee Sundquist Jr <gary.sundquist1@gmail.com>
2018-06-02 01:25:44 -0700

    Add files via upload

---
+++ src/main/java/com/list/app/model/ListItem.java
@@ -0,0 +1,52 @@
+package com.list.app.model;
+
+import java.util.Date;
+
+public class ListItem {
+
+	private String name;
+	private String contact;
+	private Date time;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the contact
+	 */
+	public String getContact() {
+		return contact;
+	}
+	/**
+	 * @param contact the contact to set
+	 */
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	/**
+	 * @return the time
+	 */
+	public Date getTime() {
+		return time;
+	}
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	
+	public String toString() {
+		return "name: " + this.name + ", contact: " + this.contact + ", time: " + this.time.toString();
+	}
+	
+}
