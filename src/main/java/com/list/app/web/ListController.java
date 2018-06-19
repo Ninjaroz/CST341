3c78236ee61f9d6e6ebd375734212aa4f4cead0f
Gary Lee Sundquist Jr <gary.sundquist1@gmail.com>
2018-06-18 20:13:43 -0700

    Add files via upload

--- src/main/java/com/list/app/web/ListController.java
+++ src/main/java/com/list/app/web/ListController.java
@@ -1,3 +1,13 @@
+/**
+ * 
+ * The List Controller manages the list page  
+ * It populates a list for a user based on the date and user currently logged in
+ * 
+ *  @author Gary
+ *  @version 1.0
+ *  @since 2018-06-18
+ */
+
 package com.list.app.web;
 
 import org.springframework.ui.Model;
@@ -8,33 +18,57 @@ import com.list.app.model.ListItems;
 import com.list.app.service.ListService;
 import java.util.Date;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import com.list.app.service.UserServiceImpl;
+import com.list.app.model.User;
 
 @Controller
 public class ListController {
 	
     @Autowired
     private ListService listService;
-
+    
+    @Autowired
+	private UserServiceImpl userService;
+    
+    /**
+     * This method loads the list page
+     * @return Returns list page
+     */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String displayList(Model model, HttpServletRequest request) {   	
     	model.addAttribute("today", new Date());
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
         	model.addAttribute("userName", authentication.getName().toString());
+        	//loads list items
+        	ListItems nList = new ListItems();
+        	nList.setDate(new Date());
+        	User user = userService.findByUsername(authentication.getName().toString());
+        	nList.setUserId(user.getUserId().intValue());
+        	List<ListItems> nListItems = listService.findListByUserIdAndDate(nList);
+        	model.addAttribute("listItems", nListItems);
         }
         return "list";
     }
     
+    /**
+     * This method checks if there is an existing list already and either creates a new list
+     * or updates an existing list
+     * @param ListItems 
+     * @return Nothing.
+     */
     @RequestMapping(value = "/addList", method = RequestMethod.POST,  headers = "Accept=application/json")
     public void addList(@RequestBody ListItems listItems) {   	
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	User user = userService.findByUsername(authentication.getName().toString());
+    	listItems.setUserId(user.getUserId().intValue());
+    	
     	List<ListItems> listExists = listService.findListByUserIdAndDate(listItems);
     	if (listExists.isEmpty()) {
     		System.out.println("record does not already exist for today....");
@@ -44,7 +78,12 @@ public class ListController {
     		listService.update(listItems);
     	}
     }
-    
+ 
+    /**
+     * This method removes a list from the database
+     * @param ListItems 
+     * @return Nothing.
+     */
     @RequestMapping(value = "/removeList", method = RequestMethod.POST)
     public void removeList(@RequestBody ListItems list) { 
     	listService.remove(list);
